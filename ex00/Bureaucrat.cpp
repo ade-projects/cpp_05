1f22275a44b29f075b215c2253499017772867ee
ade-projects <ade-projects@proton.me>
2026-09-13 18:17:28 +0200

    paused at parameterized constructor creation

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,56 @@
 /*   By: adeestev <adeestev@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 15:27:24 by adeestev          #+#    #+#             */
-/*   Updated: 2026/09/13 15:27:26 by adeestev         ###   ########.fr       */
+/*   Updated: 2026/09/13 18:15:10 by adeestev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : _name(name), _grade(grade)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooLow
+
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+	std::cout << "Parameterized constructor called for " << this->_name << std::endl;
+}
+
+
+Bureaucrat::Bureaucrat(const Bureaucrat& src) : _name(src.getName()), _grade(src.getGrade())
+{
+	std::cout << "Copy constructor called: cloning from " << src._name << std::endl;	
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& src)
+{
+	if (this != &src)
+	{
+		this->_grade = src.getGrade();
+	}
+	std::cout << "Copy assignment operator called: copy attributes from " << src._name << std::endl;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Destructor called for " << this->_name << std::endl;
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,44 @@
 /*   By: adeestev <adeestev@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 15:28:39 by adeestev          #+#    #+#             */
-/*   Updated: 2026/09/13 15:29:23 by adeestev         ###   ########.fr       */
+/*   Updated: 2026/09/13 18:15:14 by adeestev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+		unsigned int		_grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, unsigned int grade);
+		Bureaucrat(const Bureaucrat& src);
+		Bureaucrat& operator=(const Bureaucrat& src);
+		~Bureaucrat();
+
+		std::string getName() const;
+		int getGrade() const;
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		}
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		}
+}
+
+#endif
